a8e2dcb533fa5fb45cf65f1667ac26393e331bea
thestreamdesk-netizen <thestreamdesk@gmail.com>
2026-07-07 08:50:07 -0700

    Update build.gradle.kts

--- AlajoOS/android/app/build.gradle.kts
+++ AlajoOS/android/app/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
+    ndkVersion = "28.2.13676358"
     namespace = "com.example.alajoos"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+   
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -17,11 +17,9 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
-        defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
+    defaultConfig {
         applicationId = "com.example.alajoos"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -30,14 +28,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-
+}
 
 flutter {
     source = "../.."
-}
 }
