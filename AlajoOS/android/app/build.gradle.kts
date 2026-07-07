7eb3f2ba347aeb064cc14ded5b872786fa4c9dec
thestreamdesk-netizen <thestreamdesk@gmail.com>
2026-07-07 03:13:57 -0700

    Update build.gradle.kts

--- AlajoOS/android/app/build.gradle.kts
+++ AlajoOS/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.alajoos"
-    compileSdk = 34
+    compileSdk = 36
     ndkVersion = "27.0.12077973"
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
